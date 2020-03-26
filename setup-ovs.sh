5f2b83644647ab1e8a5f247c8450247a7071ebec
Petr Horacek <phoracek@redhat.com>
2020-03-26 19:59:04 +0100

    update the configuration script
    Based on some trial-error, this seems to be the right script.
    
    Signed-off-by: Petr Horacek <phoracek@redhat.com>

--- setup-ovs.sh
+++ setup-ovs.sh
@@ -10,14 +10,18 @@ secondary_profile_name=${SECONDARY_PROFILE_NAME}
 mac=$(sudo nmcli -g GENERAL.HWADDR dev show $default_device | sed -e 's/\\//g')
 
 # make bridge
-sudo nmcli conn add type ovs-bridge con-name ovs-bridge-brcnv conn.interface brcnv
-sudo nmcli conn add type ovs-port conn.interface brcnv-port master brcnv
-sudo nmcli conn add type ovs-interface conn.id brcnv-iface conn.interface brcnv master brcnv-port ipv4.method auto ipv4.dhcp-client-id "01:$mac" connection.autoconnect no
+nmcli conn add type ovs-bridge conn.interface brcnv ipv4.dhcp-client-id 01:$mac
+nmcli conn add type ovs-port conn.interface port0 master brcnv
+nmcli conn add type ovs-port conn.interface brcnv-port master brcnv
+nmcli conn add type ovs-interface conn.id brcnv-iface conn.interface brcnv master brcnv-port ipv4.method auto connection.autoconnect no
 
 # make bond
-sudo nmcli conn mod "$profile_name" connection.autoconnect no
-sudo nmcli conn mod "$secondary_profile_name" connection.autoconnect no
-sudo nmcli conn add type ovs-port conn.interface bond0 master brcnv ovs-port.bond-mode balance-slb
-sudo nmcli conn add type ethernet conn.interface $default_device master bond0
-sudo nmcli conn add type ethernet conn.interface $secondary_device master bond0
-sudo nmcli conn mod brcnv-iface connection.autoconnect yes
+nmcli conn add type ovs-port conn.interface bond0 master brcnv ovs-port.bond-mode balance-slb
+nmcli conn add type ethernet conn.interface $default_device master bond0
+nmcli conn add type ethernet conn.interface $secondary_device master bond0
+nmcli conn down "$profile_name" || true
+nmcli conn mod "$profile_name" connection.autoconnect no
+nmcli conn down "$secondary_profile_name" || true
+nmcli conn mod "$secondary_profile_name" connection.autoconnect no
+nmcli conn up brcnv-iface
+nmcli conn mod brcnv-iface connection.autoconnect yes
